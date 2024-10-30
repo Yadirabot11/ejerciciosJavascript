e2a780cf654ccacab27b5ff9cb954fdaeb1339e0
Yadirabot11 <medinaazuyadira@gmail.com>
2024-10-30 12:23:03 -0500

    feat:solucion del ejercicio 2 calcularImpuestos

--- 1.contraseñaValida/ejercicio.js
+++ 1.contraseñaValida/ejercicio.js
@@ -1,5 +1,7 @@
 // escribe tu respuesta acá
-
+function contrasenaValida(contrasena){
+    return contrasena === "2Fj(jjbFsuj" || contrasena === "eoZiugBf&g9";
+}
 // código de prueba
 console.log(contrasenaValida("2Fj(jjbFsuj")) // true
 console.log(contrasenaValida("eoZiugBf&g9")) // true
