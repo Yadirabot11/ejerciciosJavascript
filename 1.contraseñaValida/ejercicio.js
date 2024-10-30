08aba480f9d7b1c795fdd806e21e70acb7bf83ff
Yadirabot11 <medinaazuyadira@gmail.com>
2024-10-30 12:15:26 -0500

    feat:solucion del ejercicio 1 contraseñaValida

---
+++ 1.contraseñaValida/ejercicio.js
@@ -0,0 +1,7 @@
+// escribe tu respuesta acá
+
+// código de prueba
+console.log(contrasenaValida("2Fj(jjbFsuj")) // true
+console.log(contrasenaValida("eoZiugBf&g9")) // true
+console.log(contrasenaValida("hola")) // false
+console.log(contrasenaValuda("")) // false
